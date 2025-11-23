00e6498b5db4821963c4e8569d4e8650c8b67c67
Dor Serero <dor.serero@gmail.com>
2025-11-23 20:50:50 +0000

    feat(fs-restrict): support audting
    Signed-off-by: Dor Serero <dor.serero@gmail.com>

--- gadgets/fs-restrict/program.bpf.c
+++ gadgets/fs-restrict/program.bpf.c
@@ -5,7 +5,18 @@
 
 #include <vmlinux.h>
 
+#include <gadget/buffer.h>
+#include <gadget/common.h>
+#include <gadget/filesystem.h>
 #include <gadget/filter.h>
+#include <gadget/macros.h>
+
+const volatile bool enforce = true;
+GADGET_PARAM(enforce);
+
+GADGET_TRACER_MAP(events, 1024 * 256);
+
+GADGET_TRACER(fs_restrict, events, event);
 
 static __always_inline bool
 is_file_in_container_rootfs(struct task_struct *task, struct file *file) {
@@ -34,8 +45,26 @@ int BPF_PROG(micromize_bprm_creds_for_exec, struct linux_binprm *bprm) {
   task = bpf_get_current_task_btf();
   file = bprm->file;
 
-  if (!is_file_in_container_rootfs(task, file))
-    return -EPERM;
+  if (!is_file_in_container_rootfs(task, file)) {
+    struct event *event;
+    event = gadget_reserve_buf(&events, sizeof(*event));
+    if (!event)
+      return 0;
+
+    gadget_process_populate(&event->process);
+    event->timestamp_raw = bpf_ktime_get_boot_ns();
+
+    struct path f_path = BPF_CORE_READ(file, f_path);
+    char *path_str = get_path_str(&f_path);
+    if (path_str)
+      bpf_probe_read_kernel_str(event->filename, sizeof(event->filename),
+                                path_str);
+
+    gadget_submit_buf(ctx, &events, event, sizeof(*event));
+
+    if (enforce)
+      return -EPERM;
+  }
 
   return 0;
 }
--- gadgets/fs-restrict/program.bpf.h
+++ gadgets/fs-restrict/program.bpf.h
@@ -1,3 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
+/* Copyright (c) 2024 micromize-Authors */
+
+#include <gadget/common.h>
+#include <gadget/filesystem.h>
+
 #ifndef EPERM
 #define EPERM 1
 #endif
+
+struct event {
+  gadget_timestamp timestamp_raw;
+  struct gadget_process process;
+  char filename[GADGET_PATH_MAX];
+};
